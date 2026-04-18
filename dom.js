2f5f95e2214978de43576cafe6a39a9d36d75988
tanmaykapil17 <tanmay.25b01011235@abes.ac.in>
2026-04-18 23:04:26 +0530

    dom1

--- dom.js
+++ dom.js
@@ -19,10 +19,61 @@
     
 // })
 
-var box =document.getElementById('box');
+// var box =document.getElementById('box');
 
-console.log(box);
 
-box.addEventListener('click',function(){
-    box.style.color='red';
+// box.addEventListener('click',function(){
+//     box.style.color='red';
+// })
+
+// var h1 = document.querySelectorAll('h1');
+// h1[2].innerHTML='tanmay 1'
+
+// var box= document.getElementById('box');
+// //box.textContent='<h1>changed</h1>' yeh html tag ko bhi include karega
+// box.innerHTML='<h1>changed</h1>';
+
+// function changebox(){
+//     console.log("function is running-----");
+//     box.style.backgroundColor='red';
+    
+// }
+
+// var button= document.querySelector('button');
+// var box= document.querySelector('#box')
+
+// // button.addEventListener('click',function(){
+// //     console.log("Hello");
+// //     box.style.backgroundColor ='red'
+// // })
+// button.addEventListener('click',changebox);
+
+
+
+
+//function
+
+var btn= document.querySelector('button');
+var h5= document.querySelector('h5')
+
+var check=0;
+btn.addEventListener('click',function(){
+    if(check==0){
+        h5.innerHTML='friends'
+    h5.style.color='blue'
+    h5.style.fontSize='15px'
+    btn.innerHTML='remove friend'
+    console.log("friendship done ");
+    
+    check=1;
+    }
+
+    else{
+    h5.innerHTML='stranger'
+    h5.style.color='red'
+    h5.style.fontSize='15px'
+    btn.innerHTML='add friend'
+    console.log("friendship tut gayi ")
+    check=0;                
+    }
 })
